78699e03b32018751ad7d94c222e610b956d068b
Hammad Khan <hammadk42623@gmail.com>
2025-02-27 01:33:17 +0500

    biography page

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -12,6 +12,7 @@ import { useRef } from "react";
 import Image from 'next/image';
 import ProductCard from '../../components/card/ProductCard';
 import JobsCard from "@/components/card/JobsCard";
+import BiographyCard from "@/components/card/BiographyCard";
 
 
 export default function Home() {
@@ -175,8 +176,8 @@ export default function Home() {
             </button>
             <Swiper
               modules={[Autoplay]}
-              spaceBetween={80}
-              slidesPerView={2}
+              spaceBetween={40}
+              slidesPerView={5}
               onSwiper={(swiper) => (swiperRef.current = swiper)}
               autoplay={{ delay: 3000, disableOnInteraction: false }}
               loop={true}
@@ -184,7 +185,7 @@ export default function Home() {
             >
               {[...Array(7)].map((_, index) => (
                 <SwiperSlide key={index}>
-                  <JobsCard />
+                  <BiographyCard />
                 </SwiperSlide>
               ))}
             </Swiper>
@@ -198,6 +199,118 @@ export default function Home() {
         </ContentCard>
 
       </section>
+
+      <HoverBanner />
+
+      <section className="px-32 py-8">
+        <SliderSection heading="Cars" />
+        <ContentCard>
+          <div className="relative w-full mt-8   h-fit">
+            <button
+              onClick={() => swiperRef.current?.slidePrev()}
+              className="absolute left-[-55px] bg-[#d1d1d1] top-1/2 transform -translate-y-1/2 border border-[#ff0000] flex items-center justify-center text-white p-1 rounded-full object-contain z-10"
+            >
+              <Image src={"/website/assets/images/icons/slider.png"} alt="prev-icon" width={1000} height={1000} className="h-7 w-7 object-contain rotate-180" />
+            </button>
+            <Swiper
+              modules={[Autoplay]}
+              spaceBetween={40}
+              slidesPerView={5}
+              onSwiper={(swiper) => (swiperRef.current = swiper)}
+              autoplay={{ delay: 3000, disableOnInteraction: false }}
+              loop={true}
+              className="w-full min-h-40"
+            >
+              {[...Array(7)].map((_, index) => (
+                <SwiperSlide key={index}>
+                  <ProductCard />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <button
+              onClick={() => swiperRef.current?.slideNext()}
+              className="absolute right-[-55px] bg-[#d1d1d1] top-1/2 transform -translate-y-1/2 border border-[#ff0000] flex items-center justify-center text-white p-1 rounded-full object-contain z-10"
+            >
+              <Image src={"/website/assets/images/icons/slider.png"} alt="prev-icon" width={1000} height={1000} className="h-7 w-7 object-contain " />
+            </button>
+          </div>
+        </ContentCard>
+
+      </section>
+
+      <section className="px-32 py-8">
+        <SliderSection heading="Bikes" />
+        <ContentCard>
+          <div className="relative w-full mt-8   h-fit">
+            <button
+              onClick={() => swiperRef.current?.slidePrev()}
+              className="absolute left-[-55px] bg-[#d1d1d1] top-1/2 transform -translate-y-1/2 border border-[#ff0000] flex items-center justify-center text-white p-1 rounded-full object-contain z-10"
+            >
+              <Image src={"/website/assets/images/icons/slider.png"} alt="prev-icon" width={1000} height={1000} className="h-7 w-7 object-contain rotate-180" />
+            </button>
+            <Swiper
+              modules={[Autoplay]}
+              spaceBetween={40}
+              slidesPerView={5}
+              onSwiper={(swiper) => (swiperRef.current = swiper)}
+              autoplay={{ delay: 3000, disableOnInteraction: false }}
+              loop={true}
+              className="w-full min-h-40"
+            >
+              {[...Array(7)].map((_, index) => (
+                <SwiperSlide key={index}>
+                  <ProductCard />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <button
+              onClick={() => swiperRef.current?.slideNext()}
+              className="absolute right-[-55px] bg-[#d1d1d1] top-1/2 transform -translate-y-1/2 border border-[#ff0000] flex items-center justify-center text-white p-1 rounded-full object-contain z-10"
+            >
+              <Image src={"/website/assets/images/icons/slider.png"} alt="prev-icon" width={1000} height={1000} className="h-7 w-7 object-contain " />
+            </button>
+          </div>
+        </ContentCard>
+
+      </section>
+      <section className="px-32 py-8">
+        <SliderSection heading="Zip Codes" />
+        <ContentCard>
+          <div className="relative w-full mt-8   h-fit">
+            <button
+              onClick={() => swiperRef.current?.slidePrev()}
+              className="absolute left-[-55px] bg-[#d1d1d1] top-1/2 transform -translate-y-1/2 border border-[#ff0000] flex items-center justify-center text-white p-1 rounded-full object-contain z-10"
+            >
+              <Image src={"/website/assets/images/icons/slider.png"} alt="prev-icon" width={1000} height={1000} className="h-7 w-7 object-contain rotate-180" />
+            </button>
+            <Swiper
+              modules={[Autoplay]}
+              spaceBetween={40}
+              slidesPerView={5}
+              onSwiper={(swiper) => (swiperRef.current = swiper)}
+              autoplay={{ delay: 3000, disableOnInteraction: false }}
+              loop={true}
+              className="w-full min-h-40"
+            >
+              {[...Array(7)].map((_, index) => (
+                <SwiperSlide key={index}>
+                  <ProductCard />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <button
+              onClick={() => swiperRef.current?.slideNext()}
+              className="absolute right-[-55px] bg-[#d1d1d1] top-1/2 transform -translate-y-1/2 border border-[#ff0000] flex items-center justify-center text-white p-1 rounded-full object-contain z-10"
+            >
+              <Image src={"/website/assets/images/icons/slider.png"} alt="prev-icon" width={1000} height={1000} className="h-7 w-7 object-contain " />
+            </button>
+          </div>
+        </ContentCard>
+
+      </section>
+
+
+      <HoverBanner />
     </div>
 
   )
---
+++ components/card/BiographyCard.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+function BiographyCard() {
+    return (
+        <div className='border rounded-2xl border-black p-4 flex items-center justify-center flex-col bg-white gap-3'>
+            <Image src={'/website/assets/images/biography/01.png'} width={1000} height={1000} alt="product" className="h-36  w-44 rounded-2xl" />
+            <div className='divider h-[1px] w-full bg-black'></div>
+            <h6>Dilraba Dilmurat</h6>
+        </div>
+    )
+}
+
+export default BiographyCard
