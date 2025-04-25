1538af96fae06d8f6fa5b90ecadd1090ed756b41
Sameer Ali <alisameer52718@gmail.com>
2025-04-25 18:55:38 +0500

    code added for ads tera

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -5,15 +5,11 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { ToastContainer } from "react-toastify";
-import Head from "next/head";
 
 // app/layout.js
 export default function WebsiteLayout({ children }) {
   return (
     <>
-      <Head>
-        <meta name="adsterraverification" content="92gh47g92" />
-      </Head>
       <Header />
       <main className="flex-1 bg-white">{children}</main>
       <Footer />
