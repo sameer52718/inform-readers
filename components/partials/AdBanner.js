fcb6498ddbe5536ce2a33be05cf4464a5b726f71
Sameer Ali <alisameer52718@gmail.com>
2025-04-27 16:18:44 +0500

    fixe

--- components/partials/AdBanner.js
+++ components/partials/AdBanner.js
@@ -8,9 +8,6 @@ function AdBanner() {
       className="w-[728px] h-[90px] mx-auto my-4 border border-gray-200 text-white text-center "
       style={{ minHeight: "90px" }}
     >
-      <div className="absolute inset-0 flex items-center justify-center bg-gray-100 text-gray-500">
-        Loading Ad...
-      </div>
       <Script id="ad-script-options" strategy="afterInteractive">
         {`
           atOptions = {
