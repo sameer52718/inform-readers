56ebe6bd4ce54efac1852051b5b6c9df26b6748a
Sameer Ali <alisameer52718@gmail.com>
2025-04-27 16:10:42 +0500

    fixes

--- components/partials/AdBanner.js
+++ components/partials/AdBanner.js
@@ -1,9 +1,16 @@
+"use client";
 import Script from "next/script";
 import React from "react";
 
 function AdBanner() {
   return (
-    <div className="w-[728px] h-[90px] mx-auto my-4 border border-gray-200 text-white text-center ">
+    <div
+      className="w-[728px] h-[90px] mx-auto my-4 border border-gray-200 text-white text-center "
+      style={{ minHeight: "90px" }}
+    >
+      <div className="absolute inset-0 flex items-center justify-center bg-gray-100 text-gray-500">
+        Loading Ad...
+      </div>
       <Script id="ad-script-options" strategy="afterInteractive">
         {`
           atOptions = {
@@ -26,38 +33,3 @@ function AdBanner() {
 }
 
 export default AdBanner;
-
-// "use client";
-// import { useEffect, useRef } from "react";
-
-// export default function Banner() {
-//   const banner = useRef(null);
-
-//   const atOptions = {
-//     key: "3601496aca75aa7c85a600dae63104d0",
-//     format: "iframe",
-//     height: 90,
-//     width: 728,
-//     params: {},
-//   };
-
-//   useEffect(() => {
-//     if (banner.current && !banner.current.firstChild) {
-//       const conf = document.createElement("script");
-//       const script = document.createElement("script");
-//       script.type = "text/javascript";
-//       script.src = `//www.highperformancedformats.com/${atOptions.key}/invoke.js`;
-//       conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
-
-//       banner.current.appendChild(conf);
-//       banner.current.appendChild(script);
-//     }
-//   }, [banner]); // <-- No need to add [banner]
-
-//   return (
-//     <div
-//       className="mx-2 my-5 border border-gray-200 flex justify-center items-center text-white text-center"
-//       ref={banner}
-//     />
-//   );
-// }
