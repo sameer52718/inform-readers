4a4983f51ab9aa7b403310dfa9c492e028396ed7
Sameer Ali <alisameer52718@gmail.com>
2025-05-18 22:20:35 +0500

    new page deisplayed

--- app/(website)/(news)/news/page.js
+++ app/(website)/(news)/news/page.js
@@ -1,26 +1,55 @@
 "use client";
 
-import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Search, ChevronDown, ChevronUp, Clock, User, Tag, ArrowRight } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
+import handleError from "@/lib/handleError";
+import axiosInstance from "@/lib/axiosInstance";
+import getTimeAgo from "@/lib/fromNow";
 
 export default function NewsPage() {
-  const [activeTab, setActiveTab] = useState("Latest");
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [pagination, setPagination] = useState({
+    totalItems: 0,
+    currentPage: 1,
+    totalPages: 1,
+    pageSize: 10,
+  });
 
-  const tabs = ["Latest", "Popular", "Technology", "Business", "Entertainment"];
+  const getData = useCallback(async (page, limit) => {
+    try {
+      setIsLoading(true);
+      const { data } = await axiosInstance.get("/website/article", { params: { page, limit } });
+      if (!data.error) {
+        setData((prev) => [...prev, ...data.categories]);
+        setPagination(data.pagination);
+      }
+    } catch (error) {
+      handleError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+  useEffect(() => {
+    getData(pagination.currentPage, pagination.pageSize);
+  }, [pagination.currentPage, pagination.pageSize]);
+
+  const loadMore = () => {
+    setPagination((prev) => ({ ...prev, currentPage: pagination.currentPage + 1 }));
+  };
 
   return (
     <main className="min-h-screen bg-gray-50">
       {/* Hero Section */}
-      <section className="bg-white border-b">
+      {/* <section className="bg-white border-b">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            {/* Featured Article */}
             <div className="relative group">
               <div className="relative h-[400px] rounded-xl overflow-hidden">
                 <Image
-                  src="https://images.pexels.com/photos/518543/pexels-photo-518543.jpeg"
+                  src="/website/assets/images/fallback/news2.png"
                   alt="Featured news"
                   fill
                   className="object-cover group-hover:scale-105 transition-transform duration-300"
@@ -46,13 +75,12 @@ export default function NewsPage() {
               </div>
             </div>
 
-            {/* Secondary Articles */}
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
               {[1, 2, 3, 4].map((item) => (
                 <div key={item} className="group">
                   <div className="relative h-[180px] rounded-xl overflow-hidden">
                     <Image
-                      src={`https://images.pexels.com/photos/51854${item}/pexels-photo-51854${item}.jpeg`}
+                      src={`/website/assets/images/fallback/news2.png`}
                       alt={`News ${item}`}
                       fill
                       className="object-cover group-hover:scale-105 transition-transform duration-300"
@@ -72,29 +100,13 @@ export default function NewsPage() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* News Categories */}
       <section className="py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {/* Category Tabs */}
-          <div className="flex items-center justify-between border-b border-gray-200 mb-8">
-            <div className="flex space-x-8">
-              {tabs.map((tab) => (
-                <button
-                  key={tab}
-                  onClick={() => setActiveTab(tab)}
-                  className={`pb-4 text-sm font-medium transition-colors ${
-                    activeTab === tab
-                      ? "text-red-600 border-b-2 border-red-600"
-                      : "text-gray-500 hover:text-gray-700"
-                  }`}
-                >
-                  {tab}
-                </button>
-              ))}
-            </div>
-
+          {/* <div className="flex items-center justify-between border-b border-gray-200 mb-8">
             <div className="relative">
               <input
                 type="text"
@@ -103,55 +115,52 @@ export default function NewsPage() {
               />
               <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
             </div>
-          </div>
+          </div> */}
 
           {/* News Grid */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {[...Array(6)].map((_, index) => (
+            {data.map((item, index) => (
               <article key={index} className="bg-white rounded-xl shadow-sm overflow-hidden group">
                 <div className="relative h-48">
                   <Image
-                    src={`https://images.pexels.com/photos/518${index + 521}/pexels-photo-518${
-                      index + 521
-                    }.jpeg`}
+                    src={`/website/assets/images/fallback/news2.png`}
                     alt={`Article ${index + 1}`}
                     fill
                     className="object-cover group-hover:scale-105 transition-transform duration-300"
                   />
-                  <div className="absolute top-4 left-4">
+                  <div className="absolute space-x-2 top-4 left-4">
                     <span className="bg-red-600 text-white text-xs font-semibold px-3 py-1 rounded-full">
-                      Technology
+                      {item?.category?.name}
+                    </span>
+                    <span className="bg-primary-600 text-white text-xs font-semibold px-3 py-1 rounded-full">
+                      {item?.source}
                     </span>
                   </div>
                 </div>
 
                 <div className="p-6">
-                  <h3 className="text-xl font-semibold mb-2 group-hover:text-red-600 transition-colors">
-                    The Rise of Remote Work and Its Impact on Modern Society
+                  <h3 className="text-xl line-clamp-2 font-semibold mb-2 group-hover:text-red-600 transition-colors">
+                    {item.title}
                   </h3>
 
-                  <p className="text-gray-600 mb-4 line-clamp-2">
-                    Exploring how the shift to remote work is changing workplace dynamics and urban
-                    development...
-                  </p>
+                  <p className="text-gray-600 mb-4 line-clamp-2">{item.content}</p>
 
                   <div className="flex items-center justify-between text-sm text-gray-500">
                     <div className="flex items-center space-x-4">
                       <span className="flex items-center">
-                        <Clock className="h-4 w-4 mr-1" />2 hours ago
-                      </span>
-                      <span className="flex items-center">
-                        <Tag className="h-4 w-4 mr-1" />5 min read
+                        <Clock className="h-4 w-4 mr-1" />
+                        {getTimeAgo(item.pubDate)}
                       </span>
                     </div>
 
-                    <Link
-                      href={`/news/${index + 1}`}
+                    <a
+                      href={item.link}
                       className="flex items-center text-red-600 font-medium hover:text-red-700"
+                      target="_blank"
                     >
                       Read more
                       <ArrowRight className="h-4 w-4 ml-1" />
-                    </Link>
+                    </a>
                   </div>
                 </div>
               </article>
@@ -159,12 +168,18 @@ export default function NewsPage() {
           </div>
 
           {/* Load More Button */}
-          <div className="text-center mt-12">
-            <button className="bg-red-600 text-white px-6 py-3 rounded-full font-medium hover:bg-red-700 transition-colors inline-flex items-center">
-              Load more articles
-              <ChevronDown className="h-5 w-5 ml-2" />
-            </button>
-          </div>
+          {pagination.totalPages !== pagination.currentPage && (
+            <div className="text-center mt-12">
+              <button
+                className="bg-red-600 text-white px-6 py-3 rounded-full font-medium hover:bg-red-700 transition-colors inline-flex items-center"
+                onClick={loadMore}
+                disabled={isLoading}
+              >
+                {isLoading ? "Loading.." : "Load more articles"}
+                <ChevronDown className="h-5 w-5 ml-2" />
+              </button>
+            </div>
+          )}
         </div>
       </section>
     </main>
