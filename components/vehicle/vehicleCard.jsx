d4bea6ec838995de84a136b07a3a735387127f59
Sameer Ali <alisameer52718@gmail.com>
2025-12-06 20:47:40 +0500

    fix

--- components/vehicle/vehicleCard.jsx
+++ components/vehicle/vehicleCard.jsx
@@ -59,7 +59,7 @@ const VehicleCard = ({ vehicle, type = "cars" }) => {
               {vehicle.vehicleType.charAt(0) + vehicle.vehicleType.slice(1).toLowerCase()}
             </span>
             <Link
-              href={`/${type}/${vehicle?.makeId?.slug}/${vehicle.slug}`}
+              href={`/${type}/${vehicle?.makeId?.slug || vehicle?.make?.slug}/${vehicle.slug}`}
               className="text-white bg-red-600 hover:bg-red-700 px-6 py-2 rounded-lg transition-colors"
             >
               View Details
